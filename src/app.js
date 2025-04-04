09fb4238572b07f9ccd7e9a87c902455c782a0c4
scanpe <s.candelario.p@gmail.com>
2025-04-04 13:46:40 +0100

    Interactividad con JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+window.onload = function() { //esperamos a que carge la página antes de empezar
+    var formulario = document.getElementById('formulario_suscripcion'); //busco el id del formulario de la página index.html
+    
+    formulario.onsubmit = function() { //cuando le aprenten al botón de suscribirse:
+        event.preventDefault(); //para q no se envie antes de tiempo
+        let nombre = document.getElementById('nombre').value; //así se lee el valor que han puesto del campo en concreto
+        let correo = document.getElementById('correo').value;
+        let politica = document.getElementById('politica').checked; //aqui si apretaron al check de la poltica!
+        let mensaje = document.getElementById('mensaje');
+
+        if (nombre !== "" && correo !== "" && politica) { 
+            mensaje.textContent = "¡Muchas gracias por suscribirte a nuestro Blog!";
+            mensaje.className = "success"; //para css
+            mensaje.style.display = "block";
+        } 
+    };
+};
